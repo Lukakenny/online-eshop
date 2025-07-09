71ac321d1fa5fdff01525b02ad334ec89bfa3c66
Lukakenny <lukakeni95@gmail.com>
2025-07-09 13:53:03 +0200

    all solve

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import CardComponent from "../components/cardComponent";
+import CardComponent from "../components/CardComponent";
 
 function FavoritePage() {
   const { allFavorite } = useSelector((state) => state.favoriteStore);
